16a0456464c466a77a8ce6eb0d0280d09d941753
jnelson18 <jacobnelson18@gmail.com>
2022-07-05 18:44:28 +0200

    Update __init__.py to v0.1.0

--- pyquantrf/__init__.py
+++ pyquantrf/__init__.py
@@ -1,7 +1,7 @@
 from pyquantrf.core import QuantileRandomForestRegressor
 
 __title__ = 'pyquantrf'
-__version__ = '0.0.3'
+__version__ = '0.1.0'
 __author__ = 'Jacob A. Nelson'
 __author_email__ = 'jnelson@bgc-jena.mpg.de'
 __license__ = 'GNU GPLv3'
